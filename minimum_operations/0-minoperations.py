bfe86112fdd542501f1ee8bf4d799d228263d608
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-23 12:29:47 -0400

    docs: add docstring to minOperations function for clarity and detail

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 
+"""
+Find min operations of n H in a text file
+"""
+
+
 def minOperations(n) -> int:
+    """
+    Calculates the minimum number of operations needed to result in exactly n 'H' characters in a text file,
+    starting with a single 'H'. The only operations allowed are Copy All and Paste.
+    Args:
+        n (int): The target number of 'H' characters.
+    Returns:
+        int: The minimum number of operations required to achieve exactly n 'H' characters.
+              Returns 0 if n is less than 2.
+    """
+
     if n < 2:
         return 0
-    
+
     operations = 0
     factor = 2
 
