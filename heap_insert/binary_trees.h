f26a2af4246b3a22c0c19e09f6771ac2ae796137
SergioLebron22 <lebronsergio22@gmail.com>
2025-02-16 13:51:10 -0400

    feat: define binary tree structure and add heap insertion functions

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -1,3 +1,7 @@
+#ifndef binary_trees_h
+#define binary_trees_h
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -6,10 +10,18 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
+}binary_tree_t;
+
+typedef struct binary_tree_s heap_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *heapify(heap_t *node);
+
+#endif
